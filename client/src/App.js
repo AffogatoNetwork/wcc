5a0395f42fd202e4c4c2d7b1d3184e68d62b9381
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2019-11-10 14:30:42 -0600

    Setup of the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,110 @@
 import React from "react";
-import logo from "./logo.svg";
+import getWeb3 from './utils/getWeb3';
+import TokenHandler from "./contracts/CoffeeHandler.json";
+import { ThemeProvider } from 'styled-components';
+import { theme, Box, Card } from 'rimble-ui';
+import { NavLink } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+
+import NetworkIndicator from '@rimble/network-indicator';
+import ConnectionBanner from '@rimble/connection-banner';
+
+import Header from './components/header.js';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requiredNetwork: 4,
+    }
+  }
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = TokenHandler.networks[networkId];
+      console.log(`network id => ${networkId}`)
+      this.setState({
+        networkId: networkId
+      });
+
+      const contract = new web3.eth.Contract(
+        TokenHandler.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      const context = this;
+      window.ethereum.on('accountsChanged', function (accounts) {
+        context.setState({ account: accounts[0].toLowerCase() });
+      })
+
+      const owner = await contract.methods.owner().call();
+      console.log(owner);
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({
+        web3,
+        account: accounts[0].toLowerCase(),
+        contract,
+        owner: owner.toLowerCase(),
+      });
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      console.error(error);
+    }
+  }
+
+  Links = () => (
+    <Nav>
+      <NavLink activeStyle={{
+        fontWeight: "bold",
+        color: "red"
+      }} to="/validator">Validator</NavLink>
+      <NavLink activeStyle={{
+        fontWeight: "bold",
+        color: "red"
+      }} to="/producer">Producer</NavLink>
+    </Nav>
+  )
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Router>
+          <div>
+            <Header />
+            <Box maxWidth={'640px'} mx={'auto'} p={3}>
+              <this.Links />
+              <Card>
+                <NetworkIndicator currentNetwork={this.state.networkId}
+                  requiredNetwork={this.state.requiredNetwork} />
+                <ConnectionBanner
+                  currentNetwork={this.state.networkId}
+                  requiredNetwork={this.state.requiredNetwork}
+                  onWeb3Fallback={window.ethereum == null}/>
+              </Card>
+            </Box>
+          </div>
+        </Router>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/validator.js
@@ -0,0 +1,8 @@
+import { Card } from 'rimble-ui';
+import { React } from 'react';
+
+class ValidatorPage extends React.Component {
+    constructor() {
+        
+    }
+}
